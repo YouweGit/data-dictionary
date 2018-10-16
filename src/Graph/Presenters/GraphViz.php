95cb0d825f2422a9e7e8342174f2040257deab4d
Joris Ros <joris.ros@gmail.com>
2018-10-17 01:08:41 +0200

    [#6] Added a extra space so the label looks more decent

--- src/Graph/Presenters/GraphViz.php
+++ src/Graph/Presenters/GraphViz.php
@@ -255,7 +255,7 @@ class GraphViz
             $this->getView(
                 'arrow',
                 [
-                    'label' => $label
+                    'label' => '&nbsp;'.$label
                 ]
             )
         );
